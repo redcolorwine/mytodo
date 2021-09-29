79b70744d8662ac8eca410ec3b123e7fb102f0ae
Лобец Сергей Александрович <SA.Lobets.2711@fss.local>
2021-09-29 16:31:42 +1000

    commit01

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
 $(function(){
-    var $add=$('a.btask');
     var tdata=[];
     var ind;
-    var textA="";
-    var key=false;
+
 
     //Появление стандартных задач
     $('ul.menu').hide().fadeIn(2000);
@@ -20,7 +18,8 @@ $(function(){
         //Цикл по элементам хранилища
         for(var i=0;i<tdata.length;i++){
             //Создаем элементы при загрузке страницы согласно именам локального хранилища
-            var $newItem=$('<li class="vote">'+tdata[i]+'<a href="#"><img src="images/transparent-accept-icon-universal-04-icon-ok-mark-icon-5dd885e7657c34.8117789815744711434157.png" alt=""></a>'+'</li>').hide().fadeIn(1000); 
+            var $newItem=$('<li class="vote">'+tdata[i]+'<a href="#"><img src="images/arrow.png" alt=""></a>'+'</li>').hide().fadeIn(1000); 
+            
             //Если задач больше одной, то новую добавляем в конец
             if($('li.vote').length>0){
                 $('li.vote:last').after($newItem);
@@ -30,7 +29,7 @@ $(function(){
                 //ссылаемся на главное меню
                 var nMenu=$('<ul class="menu"></ul>');
                 //Вставляем перед кнопкой
-                $('a.btask').before(nMenu);
+                $('div.main').before(nMenu);
                 //После открывающегося тега вставляем элемент
                 $(nMenu).prepend($newItem);
             }
@@ -38,29 +37,18 @@ $(function(){
     }
 
     //Добавить новую задачу
-    $add.on('click',function(event){
-        //Отключаем стандартное поведение события
-        event.preventDefault();
-        //Создаем форму ввода новой задачи
-        textA=$('</br>'+'<input class="inpM" value="Введите новую задачу"></input>'+'<button class="addT">Добавить</button>').hide().fadeIn(2000);
-        //Предотвращаем повторного добавления формы
-            if($('input.inpM').length>0 && $('button.addT').length>0){
-                 alert("Кнопка уже существует!");
-                     } else {
-                         //добавляем элементы после кнопки
-                         $('a.btask').after(textA);
-            key=true;
-             }
-        
+    
         //Получаем информацию о новой задаче
         $('button.addT').on('click',function(event){
             // event.preventDefault();
+
             var $text=$('input.inpM').val();
              //Процесс создания элемента
              //Если текст существует
              if($text!=undefined){
                 //Создаем новый элемент с новым текстом
-                var $newItem=$('<li class="vote">'+$text+'<a href="#"><img src="images/transparent-accept-icon-universal-04-icon-ok-mark-icon-5dd885e7657c34.8117789815744711434157.png" alt=""></a>'+'</li>').hide().fadeIn(1000); 
+                var $newItem=$('<li class="vote">'+$text+'<a href="#"><img src="images/arrow.png" alt=""></a>'+'</li>').hide().fadeIn(1000); 
+              
                 //вставляем текст в конец строкового массива
                 tdata.push($text);
                 //сохраняем в локальном хранилище
@@ -73,14 +61,12 @@ $(function(){
             //Если задач нет, то создаем новое меню с новой задачей перед кнопкой
             else {
                 var nMenu=$('<ul class="menu"></ul>');
-                $('a.btask').before(nMenu);
+                $('div.main').before(nMenu);
                 $(nMenu).prepend($newItem);
             }
-            //после добавления элемента удаляем форму ввода
-            textA.remove();
         })
           
-    })
+   
 
     //Задача выполнена
     //При удалении задачи используем анимацию сокрытия
